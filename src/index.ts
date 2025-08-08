8920f478781f6942d4d312f3fc2a9d873ae398cb
kells <jwkim101201@gmail.com>
2025-08-08 18:42:30 +0900

    implement login

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import express from 'express'
 import proxy from 'express-http-proxy'
 import { readFileSync } from 'fs'
-
+import path from 'path'
+import bodyParser from 'body-parser'
 import jwt from 'jsonwebtoken'
+import cookieParser from 'cookie-parser'
 
 import _settings from '../settings.json'
 
 interface Settings {
+  auth: {
+    id: string
+    pw: string
+  }
   fallback: string
   port: number
   rules: {
@@ -18,18 +24,55 @@ interface Settings {
 }
 
 const app = express()
-
 const settings = _settings as Settings
-// const secret = readFileSync('./private.key')
+const secret = readFileSync('./private.key')
 
-app.use(
-  '/*path',
-  proxy((req) => {
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+
+app.use('/static', express.static('./static'))
+
+app.post('/login', (req, res) => {
+  const { id, pw } = req.body
+
+  if (settings.auth.id === id && settings.auth.pw === pw) {
+    const token = jwt.sign({ id }, secret)
+
+    res.cookie('token_proxyforme', token, {
+      httpOnly: true,
+      sameSite: 'strict'
+    })
+    const { to } = req.query
+
+    console.log(req.query)
+
+    if (to) {
+      return res.send(
+        `<script>alert('Login Successful!');location.href='${to}'</script>`
+      )
+    }
+
+    return res.send(`<script>alert('Login Successful!');</script>`)
+  }
+
+  res.send("<script>alert('Login Failed');history.back()</script>")
+})
+
+app.use((req, res, next) => {
+  if (req.path.startsWith('/static')) return next()
+
+  const token = req.cookies.token_proxyforme
+  try {
+    jwt.verify(token, secret)
+  } catch (_) {
+    return res.redirect('/static/login_pfm.html?to=' + req.path)
+  }
+
+  return proxy((req) => {
     const rule = settings.rules[req.hostname]
-    const target = rule == undefined ? settings.fallback : rule.dest
-
-    return target
-  })
-)
+    return rule ? rule.dest : settings.fallback
+  })(req, res, next)
+})
 
 app.listen(settings.port)
